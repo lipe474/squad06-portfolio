8803199855ee658dd2ff03471d2ced6cc9eccc27
davi-aguiar <daviaguiar2010@hotmail.com>
2023-08-23 12:03:40 -0300

    fixed

--- felipe/script.js
+++
@@ -1,7 +0,0 @@
-const typed = new Typed(".multiple-text", {
-  strings: ["Software Developer", "Backend Developer"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true
-})
--- luan/script.js
+++
@@ -1,7 +0,0 @@
-const typed = new Typed(".multiple-text", {
-  strings: ["Software Developer", "Web Developer", "Mobile Developer", "UI/UX Designer", "Graphic Designer", "Video Editor"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true
-})

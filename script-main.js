7c371129bb1cf2b0dcebe6677ffd69aae3847fb7
Felipe <lipe.ferreira2002@gmail.com>
2023-08-29 18:40:06 -0300

    feat: toggle menu

--- script-main.js
+++ script-main.js
@@ -1,27 +1,48 @@
-const membersSelect = document.getElementById('members');
+function redirectPage() {
+  const membersSelect = document.getElementById('members');
 
-membersSelect.addEventListener('change', function () {
-  const selectedValue = membersSelect.value;
+  membersSelect.addEventListener('change', function () {
+    const selectedValue = membersSelect.value;
 
-  if (selectedValue === 'davi') {
-    window.location.href = 'davi/index.html';
-  }
+    if (selectedValue === 'davi') {
+      window.location.href = 'davi/index.html';
+    }
 
-  if (selectedValue === 'felipe') {
-    window.location.href = 'felipe/index.html';
-  }
+    if (selectedValue === 'felipe') {
+      window.location.href = 'felipe/index.html';
+    }
 
-  if (selectedValue === 'fernanda') {
-    window.location.href = 'fernanda/index.html';
-  }
+    if (selectedValue === 'fernanda') {
+      window.location.href = 'fernanda/index.html';
+    }
 
-  if (selectedValue === 'luan') {
-    window.location.href = 'luan/index.html';
-  }
-});
+    if (selectedValue === 'luan') {
+      window.location.href = 'luan/index.html';
+    }
+  });
 
-membersSelect.addEventListener('change', function () {
-  if (membersSelect.value !== 'members') {
-    membersSelect.value = 'members';
-  }
-});
+  membersSelect.addEventListener('change', function () {
+    if (membersSelect.value !== 'members') {
+      membersSelect.value = 'members';
+    }
+  });
+}
+
+function toggleMenu() {
+  const menu = document.querySelector('#menu');
+
+  menu.addEventListener('click', function () {
+    const menuMobile = document.querySelector('.menu-mobile');
+    menuMobile.style.display = 'flex';
+  });
+
+  const closeMenu = document.querySelector('.close-menu');
+
+  closeMenu.addEventListener('click', function () {
+    const menuMobile = document.querySelector('.menu-mobile');
+    menuMobile.style.display = 'none';
+  });
+}
+
+redirectPage();
+toggleMenu();

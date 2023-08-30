36ce8b9073fd71a347e80e3156d4f4803f4ef934
Felipe <lipe.ferreira2002@gmail.com>
2023-08-30 13:11:25 -0300

    feat: menu in mobile screen

--- script-main.js
+++ script-main.js
@@ -29,20 +29,56 @@ function redirectPage() {
 }
 
 function toggleMenu() {
-  const menu = document.querySelector('#menu');
-
-  menu.addEventListener('click', function () {
-    const menuMobile = document.querySelector('.menu-mobile');
-    menuMobile.style.display = 'flex';
-  });
-
+  const menuButton = document.querySelector('#menu');
   const closeMenu = document.querySelector('.close-menu');
+  const menuMobile = document.querySelector('.menu-mobile');
+  const menuButtonContainer = document.querySelector('.menu-button-container');
+
+  menuMobile.style.display = 'none';
+  let isMenuOpen = false;
+
+  function openMenu() {
+    menuMobile.style.display = 'flex';
+    menuMobile.classList.add('menu-open');
+    menuMobile.classList.remove('menu-closed');
+    menuButtonContainer.style.display = 'none';
+    isMenuOpen = true;
+  }
+
+  function closeMenuFunction() {
+    menuMobile.classList.remove('menu-open');
+    menuMobile.classList.add('menu-closed');
+
+    setTimeout(() => {
+      menuMobile.style.display = 'none';
+      menuMobile.classList.remove('menu-closed');
+      menuButtonContainer.style.display = 'block';
+      isMenuOpen = false;
+    }, 500);
+  }
+
+  menuButton.addEventListener('click', function () {
+    openMenu();
+  });
 
   closeMenu.addEventListener('click', function () {
-    const menuMobile = document.querySelector('.menu-mobile');
-    menuMobile.style.display = 'none';
+    closeMenuFunction();
   });
-}
+
+  if (isMenuOpen) {
+    openMenu();
+  } else {
+    closeMenuFunction();
+  }
+
+  document.addEventListener('click', function (event) {
+    if (!menuMobile.contains(event.target) && !menuButtonContainer.contains(event.target)) {
+      if (isMenuOpen) {
+        closeMenuFunction();
+      }
+    }
+  });
+};
 
 redirectPage();
 toggleMenu();
